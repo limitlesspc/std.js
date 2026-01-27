cceab5896f57a75e2d40c1a50aa5e395322d356e
limitlesspc <48165199+limitlesspc@users.noreply.github.com>
2026-01-27 16:01:58 -0500

    docs: add docs for reverse curry

--- src/fn/reverse-curry.ts
+++ src/fn/reverse-curry.ts
@@ -9,9 +9,22 @@ export type ReverseCurry<Fn extends AnyFunction, Args = Parameters<Fn>> =
   : never;
 
 /**
- * Create a function that takes in the first parameter of another, to be used in a pipe
+ * Create a function that takes in the first parameter of another
+ * In other words, it gives you a function that when called with every parameter other than the first, will output a function that takes the first parameter only
  * @param fn
  * @returns a functions a that takes in the rest of the arguments of `fn` and returns a function that takes the first argument of `fn` to finally return the result of `fn`
+ * @example
+ * ```ts
+ * // Fused multiply-add
+ * function fma(a: number, b: number, c: number) {
+ *   return a * b + c;
+ * }
+ * const curried = reverseCurry(fma);
+ *
+ * const times3Add2Fma = curried(3, 2);
+ * times3Add2Fma(4); // 4 * 3 + 2 = 14
+ * times3Add2Fma(0); // 0 * 3 + 2 = 2
+ * ```
  */
 export const reverseCurry =
   <Fn extends AnyFunction>(fn: Fn): ReverseCurry<Fn> =>
